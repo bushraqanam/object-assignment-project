5d766058b92d01fbb1d3fdbf361226549214bf54
bushraqanam <bqanam@gmail.com>
2024-09-10 15:32:56 -0500

    Add files via upload
    assignment

--- main.ts
+++ main.ts
@@ -1,25 +1,43 @@
-//Q1) Name cases:store a person's name in a variable, and then print that person's name in lowercase,uppercase and titlecase//
-let myDaughter:string = "Alina"
-console.log(myDaughter.toLowerCase())
-console.log(myDaughter.toUpperCase())
-console.log(myDaughter.slice(0,1).toUpperCase()+myDaughter.slice(1,5))
-//Q2) store the names of a few of your  friends ina array called names prient each person's name by accesing each element in the list, one at a time//
-let names:string[]=["afaf","samia","numa","asma"]
-names.forEach((A)=>console.log(A))
-console.log(A)
-//Q3) start with the array you used in exercise 11 ,but instead of printing name print a message to them . message should be personalized with the person's name//
-let kids = []=["numa","amaan","alina","airah","aizah"]
-kids.forEach((B)=>console.log(B,"i love my kids"))
-//Q4)your own array: think of your favorite mode of trasportation such as motorcycle or cars and make a list prite a statement about these items such as "i would like to own honda"//
-let cars:string[]=["Honda","Tesla","BMW"]
-cars.forEach((Z)=>console.log(`i would like to own a ${Z}`))
-//Q5) make a guest list you want to invite. and one in staring and one in meddel and one in last of the list//
-let invite:string[]=["rabia","naseema","narmeen"]
-invite.forEach((box)=>console.log(box,"i want to invite you"))
-invite.unshift("iffath")
-console.log(invite)
-invite.splice(2,0,"wafa")
-console.log(invite)
-invite.push("alina")
-console.log(invite)
-invite.forEach((A)=>console.log(A,"i want to invite you"))
+// let marks:number = 35
+// if (marks>=90&&marks<=100){
+//     console.log("grade A++")
+// }
+// else if (marks>=80&&marks<=90){
+//     console.log("grade A")
+// }
+// else if (marks>=70||marks==80){
+//     console.log("grade B++")
+// }
+// else if (marks>=60||marks==70){
+//     console.log("grade B")
+// }
+// else if (marks>=50&&marks<60){
+//     console.log("grade C")
+// }
+// else if (marks>=40||marks==50){
+//     console.log("grade D")
+// }
+// else {
+//     console.log("fail")
+// }
+// //2nd program//
+// let friendAge:number = 45
+// if (friendAge>50&&friendAge<=60){
+//     console.log("this is my friend")
+// }
+// else if (friendAge>=65||friendAge==70){
+//     console.log("friend 1")
+// }
+// else if (friendAge>=55&&friendAge==75){
+//     console.log("friend 2")
+// }
+// else if (friendAge<=45&&friendAge>=60){
+//     console.log("friend 3")
+// }
+// else if (friendAge>=50&&friendAge>=70){
+// console.log("friend 4")
+// }
+// else if (friendAge<=40||friendAge==45){
+//     console.log("friend 5")
+// }
+
