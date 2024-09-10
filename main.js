0de4f0a1947fa55ad68d3054614130ca37ddc450
bushraqanam <bqanam@gmail.com>
2024-09-10 15:30:45 -0500

    Add files via upload
    assignment 2

--- main.js
+++ main.js
@@ -1,100 +1,36 @@
-//object practice assignment//
-var kid1 = {
-    name: "Numa",
-    age: 20,
-    gender: "female",
-    skincolor: "fair",
-    class: "studant",
-    married: true,
-    hobbies: ["baking", "teaching", "shoping", "styding"]
-};
-console.log(kid1);
-var kid2 = {
-    name: "Amaan",
-    age: 17,
-    gender: "male",
-    skincolor: "fair",
-    class: "studant",
-    marride: false,
-    hobbies: ["styding", "playing games", "outing"]
-};
-console.log(kid2);
-var kid3 = {
-    name: "Alina",
-    age: 14,
-    gender: "female",
-    skincolor: "fair",
-    class: "9th grade studant",
-    marride: false,
-    hobbies: ["playing batmenton", "reading", "outing", "baking"]
-};
-console.log(kid3);
-var kid4 = {
-    name: "Aairah",
-    age: 8,
-    gender: "female",
-    skincolor: "fair",
-    class: "3rd grade studant",
-    married: false,
-    hobbies: ["playing", "eating", "shoping", "reading"]
-};
-console.log(kid4);
-var kid5 = {
-    name: "Aaizah",
-    age: 7,
-    gender: "female",
-    skincolor: "fair",
-    class: "2nd grade studant",
-    married: false,
-    hobbies: ["playing", "outing", "painting", "coloring"]
-};
-console.log(kid5);
-var husband = {
-    name: "Feroz",
-    age: 48,
-    gender: "male",
-    skincolor: "fair",
-    profession: "Employee",
-    marride: true,
-    hobbies: ["reading", "writing", "outing", "cooking"]
-};
-console.log(husband);
-var laptop = {
-    name: "HP",
-    color: "gray",
-    year: 2023,
-    windows: 10,
-    serise: 5000,
-    screensize: "26 inches",
-    ownername: "bushra"
-};
-console.log(laptop);
-var cellphone = {
-    name: "iphone",
-    model: 10,
-    color: "black",
-    size: "9,inches",
-    screensize: 11,
-    year: 2022,
-    ownername: "bushra",
-};
-console.log(cellphone);
-var car = {
-    name: "accord",
-    companyname: "honda",
-    color: "gray",
-    year: 2012,
-    engine: "good",
-    melage: "80 percent",
-    model: "sedan",
-    ownername: "bushra",
-};
-console.log(car);
-var my_pet = {
-    name: "birds",
-    type: "parakeets",
-    age: "3 years",
-    color: "green and blue",
-    activity: ["playing", "singing", "flying", "makingnoises"]
-};
-console.log(my_pet);
+var greeting = 'Hello,${name}!;';
+console.log(greeting);
+var num = 2;
+var num1 = 3;
+console.log(Math.pow(num, num1));
+var age = 20;
+var myFirend = "Alina";
+var inroll = (true);
+console.log(age, myFirend, inroll);
+var indexOne = "numa";
+console.log(indexOne);
+indexOne = "amaan";
+console.log(indexOne);
+indexOne = "airah";
+console.log(indexOne);
+var num_2 = 6;
+console.log(num_2);
+num_2 = 20;
+console.log(num_2);
+num_2 = 15;
+console.log(num_2);
+var color = true;
+console.log(color);
+color = false;
+console.log(color);
+//concatination//
+var food = "pizza";
+var juice = "boba";
+var favColor = "pink";
+var num3 = 35;
+console.log(food, juice);
+var result = food + " " + juice + " " + favColor + " " + num3;
+console.log(result);
+//template literals//
+var A = "i like ".concat(food, ", ").concat(juice, " and my favcolor is ").concat(favColor, " \n my fav number is ").concat(num3);
+console.log(A);
