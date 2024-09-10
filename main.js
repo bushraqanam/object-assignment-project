2ccfe2d7459cc062cfa7757d352c9645ebe772d6
bushraqanam <bqanam@gmail.com>
2024-09-10 15:32:04 -0500

    Add files via upload
    3 asignments

--- main.js
+++ main.js
@@ -1,36 +1,24 @@
-var greeting = 'Hello,${name}!;';
-console.log(greeting);
-var num = 2;
-var num1 = 3;
-console.log(Math.pow(num, num1));
-var age = 20;
-var myFirend = "Alina";
-var inroll = (true);
-console.log(age, myFirend, inroll);
-var indexOne = "numa";
-console.log(indexOne);
-indexOne = "amaan";
-console.log(indexOne);
-indexOne = "airah";
-console.log(indexOne);
-var num_2 = 6;
-console.log(num_2);
-num_2 = 20;
-console.log(num_2);
-num_2 = 15;
-console.log(num_2);
-var color = true;
-console.log(color);
-color = false;
-console.log(color);
-//concatination//
-var food = "pizza";
-var juice = "boba";
-var favColor = "pink";
-var num3 = 35;
-console.log(food, juice);
-var result = food + " " + juice + " " + favColor + " " + num3;
-console.log(result);
-//template literals//
-var A = "i like ".concat(food, ", ").concat(juice, " and my favcolor is ").concat(favColor, " \n my fav number is ").concat(num3);
-console.log(A);
+//Q1) Name cases:store a person's name in a variable, and then print that person's name in lowercase,uppercase and titlecase//
+var myDaughter = "Alina";
+console.log(myDaughter.toLowerCase());
+console.log(myDaughter.toUpperCase());
+console.log(myDaughter.slice(0, 1).toUpperCase() + myDaughter.slice(1, 5));
+//Q2) store the names of a few of your  friends ina array called names prient each person's name by accesing each element in the list, one at a time//
+var names = ["afaf", "samia", "numa", "asma"];
+names.forEach(function (A) { return console.log(A); });
+//Q3) start with the array you used in exercise 11 ,but instead of printing name print a message to them . message should be personalized with the person's name//
+var kids = ["numa", "amaan", "alina", "airah", "aizah"];
+kids.forEach(function (B) { return console.log(B, "i love my kids"); });
+//Q4)your own array: think of your favorite mode of trasportation such as motorcycle or cars and make a list prite a statement about these items such as "i would like to own honda"//
+var cars = ["Honda", "Tesla", "BMW"];
+cars.forEach(function (Z) { return console.log("i would like to own a ".concat(Z)); });
+//Q5) make a guest list you want to invite. and one in staring and one in meddel and one in last of the list//
+var invite = ["rabia", "naseema", "narmeen"];
+invite.forEach(function (box) { return console.log(box, "i want to invite you"); });
+invite.unshift("iffath");
+console.log(invite);
+invite.splice(2, 0, "wafa");
+console.log(invite);
+invite.push("alina");
+console.log(invite);
+invite.forEach(function (A) { return console.log(A, "i want to invite you"); });
--- main.ts
+++ main.ts
@@ -1,37 +1,25 @@
-
-const greeting = 'Hello,${name}!;'
-console.log(greeting)
-let num:number = 2
-let num1:number = 3
-console.log(num ** num1)
-let age:number = 20
-let myFirend:string = "Alina"
-let inroll:boolean = (true) 
-console.log(age,myFirend,inroll)
-let indexOne:string = "numa"
-console.log(indexOne)
-indexOne = "amaan"
-console.log(indexOne)
-indexOne = "airah"
-console.log(indexOne)
-let num_2 = 6
-console.log(num_2)
-num_2 = 20
-console.log(num_2)
-num_2 = 15
-console.log(num_2)
-let color:boolean = true
-console.log(color)
-color = false
-console.log(color)
-//concatination//
-let food:string = "pizza"
-let juice:string = "boba"
-let favColor:string = "pink"
-let num3:number = 35
-console.log(food , juice)
-let result:string = food+" "+juice+" "+favColor+" "+num3
-console.log(result)
-//template literals//
-let A:string = `i like ${food}, ${juice} and my favcolor is ${favColor} \n my fav number is ${num3}`
+//Q1) Name cases:store a person's name in a variable, and then print that person's name in lowercase,uppercase and titlecase//
+let myDaughter:string = "Alina"
+console.log(myDaughter.toLowerCase())
+console.log(myDaughter.toUpperCase())
+console.log(myDaughter.slice(0,1).toUpperCase()+myDaughter.slice(1,5))
+//Q2) store the names of a few of your  friends ina array called names prient each person's name by accesing each element in the list, one at a time//
+let names:string[]=["afaf","samia","numa","asma"]
+names.forEach((A)=>console.log(A))
 console.log(A)
+//Q3) start with the array you used in exercise 11 ,but instead of printing name print a message to them . message should be personalized with the person's name//
+let kids = []=["numa","amaan","alina","airah","aizah"]
+kids.forEach((B)=>console.log(B,"i love my kids"))
+//Q4)your own array: think of your favorite mode of trasportation such as motorcycle or cars and make a list prite a statement about these items such as "i would like to own honda"//
+let cars:string[]=["Honda","Tesla","BMW"]
+cars.forEach((Z)=>console.log(`i would like to own a ${Z}`))
+//Q5) make a guest list you want to invite. and one in staring and one in meddel and one in last of the list//
+let invite:string[]=["rabia","naseema","narmeen"]
+invite.forEach((box)=>console.log(box,"i want to invite you"))
+invite.unshift("iffath")
+console.log(invite)
+invite.splice(2,0,"wafa")
+console.log(invite)
+invite.push("alina")
+console.log(invite)
+invite.forEach((A)=>console.log(A,"i want to invite you"))
