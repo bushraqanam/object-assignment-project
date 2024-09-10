046e99924c032f600ea5f1ccf5baee784d469db2
bushraqanam <bqanam@gmail.com>
2024-09-10 15:19:24 -0500

    Add files via upload
    object assignment

---
+++ main.ts
@@ -0,0 +1,102 @@
+//object practice assignment//
+let kid1={
+name:"Numa",
+age:20,
+gender:"female",
+skincolor:"fair",
+class:"studant",
+married:true,
+hobbies:["baking","teaching","shoping","styding"]
+
+}
+console.log(kid1)
+let kid2={
+    name:"Amaan",
+    age:17,
+    gender:"male",
+    skincolor:"fair",
+    class:"studant",
+    marride:false,
+    hobbies:["styding","playing games","outing"]
+}
+console.log(kid2)
+let kid3={
+    name:"Alina",
+    age:14,
+    gender:"female",
+    skincolor:"fair",
+    class:"9th grade studant",
+    marride:false,
+    hobbies:["playing batmenton","reading","outing","baking"]
+}
+console.log(kid3)
+let kid4 = {
+    name:"Aairah",
+    age:8,
+    gender:"female",
+    skincolor:"fair",
+    class:"3rd grade studant",
+    married:false,
+    hobbies:["playing","eating","shoping","reading"]
+}
+console.log(kid4)
+let kid5 = {
+    name:"Aaizah",
+    age:7,
+    gender:"female",
+    skincolor:"fair",
+    class:"2nd grade studant",
+    married:false,
+    hobbies:["playing","outing","painting","coloring"]
+}
+console.log(kid5)
+let husband = {
+    name:"Feroz",
+    age:48,
+    gender:"male",
+    skincolor:"fair",
+    profession:"Employee",
+    marride:true,
+    hobbies:["reading","writing","outing","cooking"]
+}
+console.log(husband)
+let laptop = {
+    name:"HP",
+    color:"gray",
+    year:2023,
+    windows:10,
+    serise:5000,
+    screensize:"26 inches",
+    ownername:"bushra"
+
+}
+console.log(laptop)
+let cellphone = {
+    name:"iphone",
+    model:10,
+    color:"black",
+    size:"9,inches",
+    screensize:11,
+    year:2022,
+    ownername:"bushra",
+}
+console.log(cellphone)
+let car = {
+    name:"accord",
+    companyname:"honda",
+    color:"gray",
+    year:2012,
+    engine:"good",
+    melage:"80 percent", 
+    model:"sedan",
+    ownername:"bushra",
+}
+console.log(car)
+let my_pet = {
+    name:"birds",
+    type:"parakeets",
+    age:"3 years",
+    color:"green and blue",
+    activity:["playing","singing","flying","makingnoises"]
+}
+console.log(my_pet)
